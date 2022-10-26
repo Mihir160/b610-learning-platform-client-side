4f0e166b9600413c048ce9cd992b8d6755b1fc1d
Mihir160 <mihir.roy266@gmail.com>
2022-10-26 18:25:37 +0600

    pdf downloaded added

--- src/Components/Courses/ParticulaCourseDeatails.js
+++ src/Components/Courses/ParticulaCourseDeatails.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import DetailCourse from './DetailCourse';
 import { FaFilePdf } from 'react-icons/fa';
+import Pdf from "react-to-pdf";
 const ParticulaCourseDeatails = () => {
     const detail = useLoaderData()
-    console.log(detail)
+    const ref = React.createRef();
     const courseDetails = detail.details
     console.log(courseDetails)
-    const { title, image_url, description,courseName_id } = detail;
+    const { title, image_url, description, courseName_id } = detail;
     return (
         <div className="m-8 grid lg:grid-cols-1 justify-items-center">
             <div className="lg:w-1/2">
@@ -15,20 +16,25 @@ const ParticulaCourseDeatails = () => {
                 <div className="card-body">
                     <div className='flex items-center justify-between'>
                         <h2 className="card-title lg:text-4xl">{title}</h2>
+
                         <label>
-                            <FaFilePdf className='h-12 w-12'></FaFilePdf>
+                            <Pdf  targetRef={ref} filename="Code-stack.pdf">
+                                {({ toPdf }) => <button onClick={toPdf}><FaFilePdf className='h-12 w-12'></FaFilePdf></button>}
+                            </Pdf>
                         </label>
                     </div>
                     <figure><img src={image_url} alt="car!" /></figure>
-                    <p>{description}</p>
-                    <h2 className='text-3xl font-bold'>Course Details</h2>
-                    <div>
-                        {
-                            courseDetails.map(detail => <DetailCourse key={detail.id} detail={detail}></DetailCourse>)
-                        }
+                    <div ref={ref}>
+                        <p>{description}</p>
+                        <h2 className='text-3xl font-bold'>Course Details</h2>
+                        <div>
+                            {
+                                courseDetails.map(detail => <DetailCourse key={detail.id} detail={detail}></DetailCourse>)
+                            }
+                        </div>
                     </div>
-                   <Link to={`/checkout/${courseName_id}`}> <button className='bg-gray-800 text-white p-2 rounded-lg'>
-                           Get premium access
+                    <Link to={`/checkout/${courseName_id}`}> <button className='bg-gray-800 text-white p-2 rounded-lg'>
+                        Get premium access
                     </button></Link>
 
                 </div>
